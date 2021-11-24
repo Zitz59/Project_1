c82993bf79ab1561e1741529f11bc4bc39414e41
Zitz59 <dondongudon@gmail.com>
2021-11-24 22:17:25 +0300

    some css added

---
+++ my-app/src/Components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+	return <header className='header'>
+		<img src="https://cdn-icons-png.flaticon.com/512/4542/4542046.png"></img>
+	</header>
+
+}
+
+export default Header;
---
+++ my-app/src/Components/Profile.jsx
@@ -0,0 +1,21 @@
+const Profile = () => {
+	return <div className='content'>
+		<div>
+			<img src="https://images.unsplash.com/photo-1614914135224-925593607248?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
+		</div>
+		<div>
+			avatar + description
+		</div>
+		<div>
+			My posts
+			<div>
+				New post
+			</div>
+			<div>post 1</div>
+			<div>post 2</div>
+		</div>
+	</div>
+
+}
+
+export default Profile;
