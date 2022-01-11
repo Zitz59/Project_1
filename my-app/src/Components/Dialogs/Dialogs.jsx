be174310ae68c6788c429fb5d109d1a486768803
Zitz59 <dondongudon@gmail.com>
2022-01-11 23:25:45 +0300

    Add layout

--- my-app/src/Components/Dialogs/Dialogs.jsx
+++ my-app/src/Components/Dialogs/Dialogs.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
+import s from './Dialogs.module.css';
 const Dialogs = (props) => {
 	return (
-		<div>
-		Dialogs
+		<div className={s.dialogs}>
+			<div className={s.dialogsItems}>
+				<div className={s.dialog}>
+					Gvidon
+				</div>
+				<div className={s.dialog + ' ' + s.active}>
+					Saltan
+				</div>
+				<div className={s.dialog}>
+					Tkachiha
+				</div>
+				<div className={s.dialog}>
+					Povariha
+				</div>
+				<div className={s.dialog}>
+					Svatya Baba-Babariha
+				</div>
+				<div className={s.dialog}>
+					Boss
+				</div>
+				<div className={s.dialog}>
+					Greek
+				</div>
+			</div>
+			<div className={s.messages}>
+				<div className={s.message}>Hi</div>
+				<div className={s.message}>Where is my money???</div>
+				<div className={s.message}>Miss you.....</div>
+				<div className={s.message}></div>
+				<div className={s.message}></div>
+			</div>
 		</div>
 	)
 	
