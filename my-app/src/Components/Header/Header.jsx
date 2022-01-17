aced805ab2afce03f67d07abbe6df7936127f8d0
Zitz59 <dondongudon@gmail.com>
2022-01-18 01:33:25 +0300

    refactor logo

--- my-app/src/Components/Header/Header.jsx
+++ my-app/src/Components/Header/Header.jsx
@@ -2,7 +2,7 @@ import s from './Header.module.css';
 
 const Header = (logo) => {
 	return <header className={s.header}>
-		<div className='logo'><img src="https://cdn-icons-png.flaticon.com/512/4542/4542046.png"></img></div>
+		<div className={s.logo}><img src="https://cdn-icons-png.flaticon.com/512/4542/4542046.png"></img></div>
 	</header>
 
 }
