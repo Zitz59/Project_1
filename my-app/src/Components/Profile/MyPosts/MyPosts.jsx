dddae438504534babed9d87576e58d27503d2f39
Zitz59 <dondongudon@gmail.com>
2022-01-18 01:27:47 +0300

    refactor MyPosts

--- my-app/src/Components/Profile/MyPosts/MyPosts.jsx
+++ my-app/src/Components/Profile/MyPosts/MyPosts.jsx
@@ -1,22 +1,26 @@
-import s from './MyPosts.module.css'
-import Post from './Post/Post';
+import s from "./MyPosts.module.css";
+import Post from "./Post/Post";
 const MyPosts = () => {
-	return (
-		<div className={s.content}>
-			<div>
-				My posts
-				<div >
-					<textarea ></textarea>
-					<button>Add Post</button>
-					<button>Remove</button>
-				</div>
-				<div className={s.posts}>
-					<Post message = "Hello , world!" likeCount = "20"/>
-					<Post message = "it's my first post!" likeCount="15"/>
-				</div>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className={s.postsBlock}>
+      <div>
+        <h3>My posts</h3>
+        <div>
+          <div>
+            <textarea></textarea>
+          </div>
+          <div className ={s.postButtons}>
+            <button>Add Post</button>
+            <button>Remove</button>
+          </div>
+        </div>
+        <div className={s.posts}>
+          <Post message="Hello , world!" likeCount="20" />
+          <Post message="it's my first post!" likeCount="15" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default MyPosts;
