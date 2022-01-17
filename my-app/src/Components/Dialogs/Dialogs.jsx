ba48bd307bf308e096c2a12f49c9214729a026c8
Zitz59 <dondongudon@gmail.com>
2022-01-18 00:51:55 +0300

    add new branch and refactor Dialogs

--- my-app/src/Components/Dialogs/Dialogs.jsx
+++ my-app/src/Components/Dialogs/Dialogs.jsx
@@ -1,37 +1,38 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import {NavLink} from 'react-router-dom';
+
+const Dialogitem = (props) => {
+	let path = "/dialogs/" + props.id;
+	
+	
+	return <div className={s.dialog + ' ' + s.active}>
+		<NavLink to={path}>{props.name}</NavLink>
+		</div>
+
+}
+
+
+const Message = (props) => {
+	return <div className = {s.dialog}>{props.message}</div>
+}
+
 const Dialogs = (props) => {
 	return (
 		<div className={s.dialogs}>
-			<div className={s.dialogsItems}>
-				<div className={s.dialog}>
-					Gvidon
-				</div>
-				<div className={s.dialog + ' ' + s.active}>
-					Saltan
-				</div>
-				<div className={s.dialog}>
-					Tkachiha
-				</div>
-				<div className={s.dialog}>
-					Povariha
-				</div>
-				<div className={s.dialog}>
-					Svatya Baba-Babariha
-				</div>
-				<div className={s.dialog}>
-					Boss
-				</div>
-				<div className={s.dialog}>
-					Greek
-				</div>
+			<div className={s.dialogsItems}>			
+				<Dialogitem name = "Gvidon" id = "1"/>
+				<Dialogitem name = "Saltan" id = "2"/>
+				<Dialogitem name = "Tkachiha" id = "3"/>
+				<Dialogitem name = "Povariha" id = "4"/>
+				<Dialogitem name = "Batman" id = "5"/>
+				<Dialogitem name = "Boss" id = "6"/>
+				<Dialogitem name = "Greek" id = "7"/>
 			</div>
 			<div className={s.messages}>
-				<div className={s.message}>Hi</div>
-				<div className={s.message}>Where is my money???</div>
-				<div className={s.message}>Miss you.....</div>
-				<div className={s.message}></div>
-				<div className={s.message}></div>
+				<Message message = "Hi"/>
+				<Message message = "Where is my money???"/>
+				<Message message = "Miss you....."/>
 			</div>
 		</div>
 	)
