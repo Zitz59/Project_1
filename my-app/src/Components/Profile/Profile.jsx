297cd8d04cbf9dd6839cae6177079bef26c01bba
Zitz59 <dondongudon@gmail.com>
2022-02-09 22:02:35 +0300

    Add props

--- my-app/src/Components/Profile/Profile.jsx
+++ my-app/src/Components/Profile/Profile.jsx
@@ -1,18 +1,18 @@
-import MyPosts from './MyPosts/MyPosts';
-import s from './Profile.module.css'
-const Profile = () => {
-	return <div>
-		<div>
-			<img src="https://images.unsplash.com/photo-1614914135224-925593607248?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
-		</div>
-		<div>
-			avatar + description
-		</div>
-		<div>
-			<MyPosts />
-		</div>
-	</div>
-
-}
+import MyPosts from "./MyPosts/MyPosts";
+import s from "./Profile.module.css";
+const Profile = (props) => {
+  return (
+    <div>
+      <div>
+        <img src="https://images.unsplash.com/photo-1614914135224-925593607248?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
+      </div>
+      <div>avatar + description</div>
+      <div>
+        <ProfileInfo />
+        <MyPosts posts={props.posts} />
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
