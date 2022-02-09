714e251632414299b0b323ed5aed9d79a6f1e0c0
Zitz59 <dondongudon@gmail.com>
2022-02-09 22:00:56 +0300

    Add props and delete data

--- my-app/src/Components/Dialogs/Dialogs.jsx
+++ my-app/src/Components/Dialogs/Dialogs.jsx
@@ -1,37 +1,17 @@
 import React from "react";
 import s from "./Dialogs.module.css";
-import { NavLink } from "react-router-dom";
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 
-//Add massive with name and id's
 const Dialogs = (props) => {
-  let dialogs = [
-    { id: 1, name: "Gvidon" },
-    { id: 2, name: "Saltan" },
-    { id: 3, name: "Tkachiha" },
-    { id: 4, name: "Povariha" },
-    { id: 5, name: "Batman" },
-    { id: 6, name: "Boss" },
-    { id: 7, name: "Greek" },
-  ];
-  // Add massive with messages
-  let messages = [
-    { id: 1, message: "Hi!" },
-    { id: 2, message: "Where is my money???" },
-    { id: 3, message: "Miss you.....!" },
-    { id: 4, message: "You are welcome!" },
-    { id: 5, message: "Backdafuckup, the Onyx is here!" },
-    { id: 6, message: "Jerry was a race car driver...." },
-    { id: 7, message: "Come to ...." },
-  ];
-  //Use .map method to dialogs massive
-  let dialogsElements = dialogs.map((dialog) => (
+  let dialogsElements = props.dialogs.map((dialog) => (
     <DialogItem name={dialog.name} id={dialog.id} />
   ));
-  //Use map method to messages massive
-  let messagesElements = messages.map((m) => <Message message={m.message} />);
-  //Add new massive with data to components
+
+  let messagesElements = props.messages.map((m) => (
+    <Message message={m.message} />
+  ));
+
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>{dialogsElements}</div>
