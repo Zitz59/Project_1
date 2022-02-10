6ee397f44b78b283a2d6883ca79048dd9a3b1136
Zitz59 <dondongudon@gmail.com>
2022-02-10 15:59:03 +0300

    Add data to state.js and refactor code

---
+++ my-app/src/Components/Redux/state.js
@@ -0,0 +1,30 @@
+let state = {
+  profilePage: {
+    posts: [
+      { id: 1, message: "Hello , world!", likeCount: 20 },
+      { id: 2, message: "It's my first post!", likeCount: 15 },
+    ],
+  },
+  dialogsPage: {
+    messages: [
+      { id: 1, message: "Hi!" },
+      { id: 2, message: "Where is my money???" },
+      { id: 3, message: "Miss you.....!" },
+      { id: 4, message: "You are welcome!" },
+      { id: 5, message: "Backdafuckup, the Onyx is here!" },
+      { id: 6, message: "Jerry was a race car driver...." },
+      { id: 7, message: "Come to ...." },
+    ],
+    dialogs: [
+      { id: 1, name: "Gvidon" },
+      { id: 2, name: "Saltan" },
+      { id: 3, name: "Tkachiha" },
+      { id: 4, name: "Povariha" },
+      { id: 5, name: "Batman" },
+      { id: 6, name: "Boss" },
+      { id: 7, name: "Greek" },
+    ],
+  },
+};
+
+export default state;
