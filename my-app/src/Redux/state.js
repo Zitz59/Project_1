86644bec5eece8f806ab4a3a2787fb03938e0576
Zitz59 <dondongudon@gmail.com>
2022-02-22 11:55:10 +0300

    Add function addPost

---
+++ my-app/src/Redux/state.js
@@ -0,0 +1,98 @@
+let state = {
+  profilePage: {
+    posts: [
+      { id: 1, message: "Hello , world!", likeCount: 20 },
+      { id: 2, message: "It's my first post!", likeCount: 15 },
+    ],
+  },
+  dialogsPage: {
+    messages: [
+      { id: 1, message: "Hi!" },
+      { id: 2, message: "Where is my money???" },
+      { id: 3, message: "Miss you.....!" },
+      { id: 4, message: "You are welcome!" },
+      { id: 5, message: "Backdafuckup, the Onyx is here!" },
+      { id: 6, message: "Jerry was a race car driver...." },
+      { id: 7, message: "Come to ...." },
+    ],
+    dialogs: [
+      {
+        id: 1,
+        name: "Gvidon",
+        avatar:
+          "https://images.generated.photos/tSejGSkqR9jNYheA_83j32i88BNeUB5ngs7U_iuSVKA/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LmNvbmQvMDBmMjU1/MzUtNzdkNy00NDcw/LWJiMWYtNDhlMDk2/YTkyZjhmLmpwZw.jpg",
+      },
+      {
+        id: 2,
+        name: "Saltan",
+        avatar:
+          "https://images.generated.photos/VPisMS5Ck8GZaYrdKHreA8enZaebUf4adQi1ijU7qTY/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LmNvbmQvMjZmZDgx/MGUtZTdlMy00ZmVl/LWEwNWQtMjM1ZmU2/ODk2ODg1LmpwZw.jpg",
+      },
+      {
+        id: 3,
+        name: "Tkachiha",
+        avatar:
+          "https://images.generated.photos/DRfn1OkhXu_AcANTk_obkJkMobp_IEURaUTEtfXgcEg/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LmNvbmQvZDMzMmY4/ZjktYTFkYy00ODZh/LWJhOGYtMDc2NDY0/ODc4ZmNiLmpwZw.jpg",
+      },
+      {
+        id: 4,
+        name: "Povariha",
+        avatar:
+          "https://images.generated.photos/DpMfkVBPxCXeCXdmIQ4SIMQW1W_B3cs5Pv5TdDjLFow/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LmNvbmQvYWQxMGYz/YjAtMDg3OC00ZWUx/LWE0ZGMtOGExOTMz/ZTBkYjY5LmpwZw.jpg",
+      },
+      {
+        id: 5,
+        name: "Batman",
+        avatar:
+          "https://images.generated.photos/oOILFtdy0xgGIYdy-Y77t34A7X0fPRCq-sHwZWjuuy0/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LmNvbmQvN2FlMTc1/YTEtM2Q5OS00ZWQ4/LWI3ZmYtMDUwOGNk/Y2FlNmNkLmpwZw.jpg",
+      },
+      {
+        id: 6,
+        name: "Boss",
+        avatar:
+          "https://images.generated.photos/5bGSGubL0ZNhOc0XYnm9CQlII5SL_B6G-xsP1duXGe8/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LmNvbmQvODI3Mzg5/MmQtOTcxOS00MTUx/LWEyYmUtNzBkOGI3/NTkzOGI4LmpwZw.jpg",
+      },
+      {
+        id: 7,
+        name: "Greek",
+        avatar:
+          "https://images.generated.photos/J2Mp46iKzYSgoMb9KFw445_3U-7sWJ0TAkpJWK4zMJQ/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LmNvbmQvYjRjOTE2/ZDYtZmNhZC00YjVj/LWFkMzItMDAyZWM2/OTllY2M1LmpwZw.jpg",
+      },
+    ],
+  },
+  sidebar: {
+    sidebarItems: [
+      {
+        id: 1,
+        name: "Fedot",
+        avatar:
+          "https://images.generated.photos/tSejGSkqR9jNYheA_83j32i88BNeUB5ngs7U_iuSVKA/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LmNvbmQvMDBmMjU1/MzUtNzdkNy00NDcw/LWJiMWYtNDhlMDk2/YTkyZjhmLmpwZw.jpg",
+      },
+      {
+        id: 2,
+        name: "Petro",
+        avatar:
+          "https://images.generated.photos/VPisMS5Ck8GZaYrdKHreA8enZaebUf4adQi1ijU7qTY/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LmNvbmQvMjZmZDgx/MGUtZTdlMy00ZmVl/LWEwNWQtMjM1ZmU2/ODk2ODg1LmpwZw.jpg",
+      },
+
+      {
+        id: 3,
+        name: "Anna",
+        avatar:
+          "https://images.generated.photos/DRfn1OkhXu_AcANTk_obkJkMobp_IEURaUTEtfXgcEg/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LmNvbmQvZDMzMmY4/ZjktYTFkYy00ODZh/LWJhOGYtMDc2NDY0/ODc4ZmNiLmpwZw.jpg",
+      },
+    ],
+  },
+};
+
+export let addPost = (postMessage) => {
+  let newPost = {
+    id: 3,
+    message: postMessage,
+    likeCount: 0,
+  };
+
+  state.profilePage.posts.push(newPost);
+};
+
+export default state;
