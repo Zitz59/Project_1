f63bfb0946e0d2d30cf9f37596b78830b8fa51e3
Zitz59 <dondongudon@gmail.com>
2022-02-22 18:15:29 +0300

    Add component ProfileInfo + props for tetxtarea

--- my-app/src/Components/Profile/Profile.jsx
+++ my-app/src/Components/Profile/Profile.jsx
@@ -1,14 +1,17 @@
 import MyPosts from "./MyPosts/MyPosts";
 import s from "./Profile.module.css";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 const Profile = (props) => {
   return (
     <div>
       <div>
-        <img src="https://images.unsplash.com/photo-1614914135224-925593607248?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
-      </div>
-      <div>avatar + description</div>
-      <div>
-        <MyPosts posts={props.posts} />
+        <ProfileInfo />
+        <MyPosts
+          posts={props.profilePage.posts}
+          newPostText={props.profilePage.newPostText}
+          updateNewPostText={props.updateNewPostText}
+          addPost={props.addPost}
+        />
       </div>
     </div>
   );
